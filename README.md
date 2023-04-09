785be8ae960ca2fdd4ed4c0d67c4d14d558b2cb8
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-04-09 09:31:52 +0600

    initial data load

---
+++ README.md
@@ -0,0 +1,1 @@
+### BAnglar T-shirt
---
+++ src/Components/OederReview/OrderReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            sgfh
+        </div>
+    );
+};
+
+export default OrderReview;
